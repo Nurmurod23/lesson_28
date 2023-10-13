58c18b0ea26390ad7dac18543c9a8ffd51bfb5b2
Nurmurod <143414526+Nurmurod23@users.noreply.github.com>
2023-10-13 02:06:41 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,198 @@
+//while_1
+
+// var N = parseInt(prompt("Xoxlagan son kiriting"));
+// var K = 0;
+
+// while(K < N){
+//     alert(K)
+//     K++
+//     break
+// }// let N = parseInt(prompt("Xoxlagan son kiriting"));
+// var K = 0;
+
+// while(K < N){
+//     alert(K)
+//     K++
+//     break
+// }
+
+
+
+
+//while_2
+
+// var A = parseInt(prompt("A uchun xoxlagan son kiriting"))
+// var B = parseInt(prompt("B uchun xoxlagan son kiriting"))
+// var counter = 0;
+
+// while(A <= B) {
+//     A++
+//     console.log(A)
+//     alert(A)
+//     alert(B)
+
+//     S+1;
+// }
+
+
+
+// While - 3
+
+// var A = parseInt(prompt("A uchun xoxlagan son kiriting"))
+// var B = parseInt(prompt("B uchun xoxlagan son kiriting"))
+// const S = 0;
+
+// while(A > B) {
+//     console.log(A)
+    
+//     A--;
+//    S+1;
+// }
+
+
+//While - 4
+
+// var A = 1500;
+
+
+// var I = 0;
+
+// while( I < 10) {
+
+//     var price = I * A
+//     console.log(price)
+//     I+1;
+// }
+
+// While - 5
+
+
+// A = 2500;
+
+
+// var I = 0;
+
+// while( I <= 1) {
+
+//     var price = I * A
+//     console.log(price.toFixed(2))
+//     I+=0.1;
+// }
+
+// While - 6
+
+
+// var A = 1000;
+
+
+// var I = 1;
+
+// while( I <= 2) {
+
+//     let price = i * A
+//     console.log(A)
+//     I += 0.2;
+// }
+
+
+
+// While - 7
+
+// const A = parseInt(prompt("A uchun xoxlagan son kiriting"))
+// var B = parseInt(prompt("B uchun xoxlagan son kiriting"))
+// var S = 0;
+
+
+// while(A < B) {
+
+//     S +=A
+//     A+1;
+// }
+
+// console.log(S)
+
+//While - 8
+
+
+// var A = parseInt(prompt("A uchun xoxlagan son kiriting"))
+// var B = parseInt(prompt("B uchun xoxlagan son kiriting"))
+// var result = 0;
+
+
+// while(A < B) {
+
+//     result *=a
+//     A+1;
+// }
+
+// console.log(result)
+
+// While - 9
+
+// let A = parseInt(prompt("A uchun xoxlagan son kiriting"))
+// var B = parseInt(prompt("B uchun xoxlagan son kiriting"))
+//var result = 0;
+
+
+// while(A < B) {
+
+//     result +=A**2
+//     A+1;
+// }
+
+// console.log(res)
+
+
+//While - 10\
+
+
+
+// var N = parseInt(prompt("N uchun xoxlagan son kiriting"))
+// var S = 0;
+
+// var I = 1;
+
+// while(I < N) {
+
+//     S += 1 / I
+
+//     I++;
+// }
+
+// console.log(sum.toFixed(2))
+
+
+// While - 11
+
+
+// var  N = parseInt(prompt("N uchun xoxlagan son kiriting"))
+// var S = 0;
+
+
+// var I = 0;
+
+// while(I < N) {
+
+//     S  = (I + I)*2* (I + I)
+
+//     I++;
+// }
+
+// console.log(sum)
+
+
+// While - 12
+
+
+// var N = parseInt(prompt("N uchun xoxlagan son kiriting"))
+// var S = 0;
+
+// var I = 1;
+
+// while(I < N) {
+
+//     S  ++ I
+
+//     I+= 0.1;
+// }
+
